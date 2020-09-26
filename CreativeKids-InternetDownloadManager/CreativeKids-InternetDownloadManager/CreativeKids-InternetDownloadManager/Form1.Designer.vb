5c20a8a7377979d4374ff6d0f1269a6c8a25c3ef
creativekids11 <70833245+creativekids11@users.noreply.github.com>
2020-09-26 18:03:10 +0530

    Add files via upload

---
+++ CreativeKids-InternetDownloadManager/CreativeKids-InternetDownloadManager/CreativeKids-InternetDownloadManager/Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.DownloadsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeafaltLocationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitWhenTheDownloadIsCompleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitDMToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DownloadsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(623, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'DownloadsToolStripMenuItem
+        '
+        Me.DownloadsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DeafaltLocationToolStripMenuItem, Me.ExitWhenTheDownloadIsCompleteToolStripMenuItem, Me.ExitDMToolStripMenuItem})
+        Me.DownloadsToolStripMenuItem.Name = "DownloadsToolStripMenuItem"
+        Me.DownloadsToolStripMenuItem.Size = New System.Drawing.Size(78, 20)
+        Me.DownloadsToolStripMenuItem.Text = "Downloads"
+        '
+        'DeafaltLocationToolStripMenuItem
+        '
+        Me.DeafaltLocationToolStripMenuItem.Name = "DeafaltLocationToolStripMenuItem"
+        Me.DeafaltLocationToolStripMenuItem.Size = New System.Drawing.Size(265, 22)
+        Me.DeafaltLocationToolStripMenuItem.Text = "Default Download Location"
+        '
+        'ExitWhenTheDownloadIsCompleteToolStripMenuItem
+        '
+        Me.ExitWhenTheDownloadIsCompleteToolStripMenuItem.Name = "ExitWhenTheDownloadIsCompleteToolStripMenuItem"
+        Me.ExitWhenTheDownloadIsCompleteToolStripMenuItem.Size = New System.Drawing.Size(265, 22)
+        Me.ExitWhenTheDownloadIsCompleteToolStripMenuItem.Text = "Exit when the download is complete"
+        '
+        'ExitDMToolStripMenuItem
+        '
+        Me.ExitDMToolStripMenuItem.Name = "ExitDMToolStripMenuItem"
+        Me.ExitDMToolStripMenuItem.Size = New System.Drawing.Size(265, 22)
+        Me.ExitDMToolStripMenuItem.Text = "Exit DM"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.ForeColor = System.Drawing.Color.Gray
+        Me.TextBox1.Location = New System.Drawing.Point(13, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(517, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "URL"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(536, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Paste"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.ForeColor = System.Drawing.Color.Gray
+        Me.TextBox2.Location = New System.Drawing.Point(88, 54)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(523, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "Filename"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "File Name: "
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(13, 80)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(456, 47)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Download File"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(475, 80)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(136, 47)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Cancel"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Impact", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(9, 199)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 19)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Satus: "
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(13, 222)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(594, 23)
+        Me.ProgressBar1.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 161)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Downloaded: --/--"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(623, 256)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "CreativeKids-DownloadManager"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents DownloadsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DeafaltLocationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitWhenTheDownloadIsCompleteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitDMToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+End Class
---
+++ CreativeKids-InternetDownloadManager/CreativeKids-InternetDownloadManager/CreativeKids-InternetDownloadManager/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Net
+
+Public Class Form1
+    Private Sub ExitDMToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitDMToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = My.Computer.Clipboard.GetText
+    End Sub
+
+    Private Sub TextBox1_Enter(sender As Object, e As EventArgs) Handles TextBox1.Enter
+        If TextBox1.Text = "URL" And TextBox1.ForeColor = Color.Gray Then
+            TextBox1.Text = ""
+            TextBox1.ForeColor = Color.Black
+        Else
+            TextBox1.SelectAll()
+        End If
+    End Sub
+
+    Private Sub TextBox1_Leave(sender As Object, e As EventArgs) Handles TextBox1.Leave
+        If TextBox1.Text = "" And TextBox1.ForeColor = Color.Black Then
+            TextBox1.Text = "URL"
+            TextBox1.ForeColor = Color.Gray
+        Else
+            TextBox1.SelectAll()
+        End If
+    End Sub
+
+    Private Sub TextBox2_Enter(sender As Object, e As EventArgs) Handles TextBox2.Enter
+        If TextBox2.Text = "Filename" And TextBox2.ForeColor = Color.Gray Then
+            TextBox2.Text = ""
+            TextBox2.ForeColor = Color.Black
+        Else
+            TextBox2.SelectAll()
+        End If
+    End Sub
+
+    Private Sub TextBox2_Leave(sender As Object, e As EventArgs) Handles TextBox2.Leave
+        If TextBox2.Text = "" And TextBox2.ForeColor = Color.Black Then
+            TextBox2.Text = "Filename"
+            TextBox2.ForeColor = Color.Gray
+        Else
+            TextBox2.SelectAll()
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text <> "URL" And TextBox1.ForeColor = Color.Gray Then
+            TextBox1.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    'Downloading
+    Private WithEvents HTTPCLIENT As WebClient
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        HTTPCLIENT = New WebClient
+        Dim Download As String = TextBox1.Text
+        Dim USER = Environment.UserName
+        Dim SAVE As String = "C:\Users\" & USER & "\Downloads\" & TextBox2.Text ''file.exe
+        Try
+            HTTPCLIENT.DownloadFileAsync(New Uri(Download), SAVE)
+            TextBox1.ReadOnly = True
+            TextBox2.ReadOnly = True
+            Button2.Enabled = False
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Information")
+            TextBox1.Text = "URL"
+            TextBox1.ForeColor = Color.Gray
+            TextBox2.Text = "Filename"
+            TextBox2.ForeColor = Color.Gray
+        End Try
+    End Sub
+
+    Private Sub HTTPCLIENT_DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs) Handles HTTPCLIENT.DownloadProgressChanged
+        ProgressBar1.Maximum = e.TotalBytesToReceive
+        ProgressBar1.Value = e.BytesReceived
+        Label3.Text = "Downloaded: " & e.BytesReceived & " / " & e.TotalBytesToReceive
+
+        If ProgressBar1.Value = ProgressBar1.Maximum Then
+            ProgressBar1.Value = 0
+            TextBox1.Text = "URL"
+            TextBox1.ForeColor = Color.Gray
+            TextBox2.Text = "Filename"
+            TextBox2.ForeColor = Color.Gray
+            TextBox1.ReadOnly = False
+            TextBox2.ReadOnly = False
+            Button2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub DeafaltLocationToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeafaltLocationToolStripMenuItem.Click
+        MessageBox.Show("The Defualt Locatio Is Downloads.", "Information")
+    End Sub
+End Class
